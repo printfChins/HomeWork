e701245fca3ecb46412e69b57842777167df0be2
printfChins <53858352+printfChins@users.noreply.github.com>
2022-10-04 23:13:16 +0800

    up

---
+++ src/Sample12.java
@@ -0,0 +1,12 @@
+public class Sample12 {
+    public static void main(String[] args){
+        int num1 = 10;
+        int num2 = 5;
+        System.out.println("num1和num2的各種運算:");
+        System.out.println("num1+num2等於"+(num1+num2));
+        System.out.println("num1+num2等於"+(num1-num2));
+        System.out.println("num1+num2等於"+(num1*num2));
+        System.out.println("num1+num2等於"+(num1/num2));
+        System.out.println("num1+num2等於"+(num1%num2));
+    }
+}
