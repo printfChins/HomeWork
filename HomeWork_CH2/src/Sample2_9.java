b08e7f73528cf7b259a477e68d5101794b0e1d76
printfChins <53858352+printfChins@users.noreply.github.com>
2022-10-27 20:23:06 +0800

    first commit

---
+++ HomeWork_CH2/src/Sample2_9.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Sample2_9 {
+    public static void main(String[] args) throws IOException {
+        for(int i=1;i<=5;i++){
+            System.out.println("第"+i+"次的迴圈");
+        }
+        System.out.println("迴圈結束");
+    }
+}
